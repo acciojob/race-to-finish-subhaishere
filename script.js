a23cfbc6b4a29ecd55b1578f15b39c245646ad56
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-25 00:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for (let i = 1; i <= 5; i++) {
+  promises.push(
+    new Promise((resolve, reject) => {
+      // Simulate some asynchronous operation
+      setTimeout(() => {
+        if (i === 3) {
+          reject(30);
+        } else {
+          resolve(30);
+        }
+      }, 1000); // Simulating 1 second delay
+    })
+  );
+}
+
+
+
+
+Promise.any(promises).then((data)=>{
+  //console.log(data);
+  document.getElementById("output").innerText=data;
+})
